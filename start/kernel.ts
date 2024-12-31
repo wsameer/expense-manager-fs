0bb5973dfbd4904df4f48d50abf296ed8f76c075
wsameer <dev.wsameer@gmail.com>
2024-12-30 23:12:04 -0500

    installs ui components from shadcn

--- start/kernel.ts
+++ start/kernel.ts
@@ -8,14 +8,14 @@
 |
 */
 
-import router from '@adonisjs/core/services/router'
-import server from '@adonisjs/core/services/server'
+import router from '@adonisjs/core/services/router';
+import server from '@adonisjs/core/services/server';
 
 /**
  * The error handler is used to convert an exception
  * to a HTTP response.
  */
-server.errorHandler(() => import('#exceptions/handler'))
+server.errorHandler(() => import('#exceptions/handler'));
 
 /**
  * The server middleware stack runs middleware on all the HTTP
@@ -28,7 +28,7 @@ server.use([
   () => import('@adonisjs/cors/cors_middleware'),
   () => import('@adonisjs/vite/vite_middleware'),
   () => import('@adonisjs/inertia/inertia_middleware'),
-])
+]);
 
 /**
  * The router middleware stack runs middleware on all the HTTP
@@ -39,7 +39,7 @@ router.use([
   () => import('@adonisjs/session/session_middleware'),
   () => import('@adonisjs/shield/shield_middleware'),
   () => import('@adonisjs/auth/initialize_auth_middleware'),
-])
+]);
 
 /**
  * Named middleware collection must be explicitly assigned to
@@ -48,4 +48,4 @@ router.use([
 export const middleware = router.named({
   guest: () => import('#middleware/guest_middleware'),
   auth: () => import('#middleware/auth_middleware'),
-})
+});
--- start/routes.ts
+++ start/routes.ts
@@ -6,6 +6,28 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+const LoginController = () => import('#controllers/auth/login_controller');
+const RegisterController = () =>
+  import('#controllers/auth/register_controller');
+import router from '@adonisjs/core/services/router';
 
-import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('welcome')
+export const ROUTES = {
+  WELCOME: '/',
+  AUTH: {
+    LOGIN: '/login',
+    REGISTER: '/register',
+  },
+} as const;
+
+router
+  .group(() => {
+    router.get('/register', [RegisterController, 'show']).as('register');
+    router.get('/login', [LoginController, 'store']).as('login');
+  })
+  .as('auth');
+
+router
+  .get(ROUTES.WELCOME, async ({ inertia }) => {
+    return inertia.render('welcome');
+  })
+  .as('welcome');
